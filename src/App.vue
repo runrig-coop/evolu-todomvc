066b75e179cf712281ff112d88e94612d109019e
Jamie Gaehring <gaehj457@gmail.com>
2024-09-21 22:36:20 -0400

    Adapt Vue TodoMVC to TypeScript.
    Based on example from Vue docs (retrieved 2024-09-18):
    https://vuejs.org/examples/#todomvc

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,16 @@
 <script setup lang="ts">
 import { createEvolu } from '@evolu/common-web';
 import { Database } from './database';
-import HelloWorld from './components/HelloWorld.vue';
+import TodoMvc from './components/TodoMvc.vue';
 
 createEvolu(Database);
 
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <TodoMvc :todos="[]" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+@import "https://unpkg.com/todomvc-app-css@2.4.1/index.css";
 </style>
